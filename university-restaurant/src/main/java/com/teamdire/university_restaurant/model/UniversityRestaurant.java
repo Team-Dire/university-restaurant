6c1641d07b859fc94b7aabb0e48919572a8f24e4
Salies <serezane@protonmail.com>
2023-01-26 00:46:03 -0300

    feat: autenticação backend

---
+++ university-restaurant/src/main/java/com/teamdire/university_restaurant/model/UniversityRestaurant.java
@@ -0,0 +1,45 @@
+package com.teamdire.university_restaurant.model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel Serezane (Salies)
+ */
+
+/*
+* Equivalente ao UniRestaurant do projeto.
+* Preferi a escrita por extenso.
+*/
+public class UniversityRestaurant {
+    private static ArrayList<Usuario> usuarios = new ArrayList<>();
+    private static Usuario usuarioAutenticado = null;
+
+    public static void addUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+    }
+
+    /**
+     * Abordagem um pouco diferente da sugerida pelos diagramas de colaboração
+     * do projeto. Separei a recuperação da autenticação.
+     */
+    public static Usuario recuperarUsuario(String cpf) {
+        for (Usuario usuario : usuarios) {
+            if (usuario.getCpf().equals(cpf)) {
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    public static boolean autenticarUsuario(String cpf, String senha) {
+        Usuario usuario = recuperarUsuario(cpf);
+        if (usuario == null || !usuario.getSenha().equals(senha)) return false;
+        usuarioAutenticado = usuario;
+        return true;
+    }
+
+    public static Usuario getUsuarioAutenticado() {
+        return usuarioAutenticado;
+    }
+}
