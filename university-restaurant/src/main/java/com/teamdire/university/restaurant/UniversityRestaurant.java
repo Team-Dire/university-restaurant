43c69504e2d26dc2816118d008c63c799726d5c9
Salies <serezane@protonmail.com>
2023-01-25 22:32:40 -0300

    feat: netbeans project

---
+++ university-restaurant/src/main/java/com/teamdire/university/restaurant/UniversityRestaurant.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.teamdire.university.restaurant;
+
+/**
+ *
+ * @author Daniel
+ */
+public class UniversityRestaurant {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
