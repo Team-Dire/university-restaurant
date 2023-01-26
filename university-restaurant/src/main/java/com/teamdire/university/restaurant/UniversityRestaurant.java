167de3b861725cfb78402d8b950db3b82570823c
Salies <serezane@protonmail.com>
2023-01-25 22:50:05 -0300

    feat: integração intellij

--- university-restaurant/src/main/java/com/teamdire/university/restaurant/UniversityRestaurant.java
+++ university-restaurant/src/main/java/com/teamdire/university/restaurant/UniversityRestaurant.java
@@ -8,8 +8,12 @@ package com.teamdire.university.restaurant;
  *
  * @author Daniel
  */
-public class UniversityRestaurant {
 
+/*
+* Equivalente ao UniRestaurant do projeto.
+* Preferi a escrita por extenso.
+*/
+public class UniversityRestaurant {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
